e0cc8ecd0612439220f16afb7447a60c8da652ea
Movies <leonardlamparter@gmail.com>
2023-10-07 17:35:16 -0400

    Added login & home screen updated to login, and fixed bugs

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-document.querySelectorAll('[remLee]').forEach(e => {
-    e.hidden = true
-})
-
-document.querySelectorAll('[remMarg]').forEach(e => {
-    e.hidden = true
-})
-
-function show(hurricaneName) {
-    document.getElementById(`remCh${hurricaneName}`).innerHTML=='Show More' ?
-        (
-            document.getElementById(`remCh${hurricaneName}`).innerHTML='Show Less',
-            document.querySelectorAll(`[rem${hurricaneName}]`).forEach(e => {
-                e.hidden = false
-            })
-        ) :
-        (
-            document.getElementById(`remCh${hurricaneName}`).innerHTML='Show More',
-            document.querySelectorAll(`[rem${hurricaneName}]`).forEach(e => {
-                e.hidden = true
-            })
-        )
-}
-
 setTimeout(() => {
     document.body.hidden = false
     document.querySelector('html').style.backgroundImage = "none";
-}, 400)
+}, 0)
+
+
+var threats = [
+    [
+        ["Bermuda", "East USA", "Canada"]
+    ],
+    {
+        "Philippe" : ["Bermuda", "East USA", "Canada"]
+    }
+]
+
+const username = location2.attributes["usrnm"]
+const loc = location2.attributes["loc"]
+
+if (username != undefined && loc != undefined) {
+    var threatsToLoc = 0
+    threats[0].forEach(stormthreats => {
+        threatsToLoc += (stormthreats.includes(loc) ? 1 : 0) 
+    })
+
+    document.querySelectorAll("[usrloc]").forEach(elm => {
+        elm.innerHTML = elm.innerHTML.toString()
+            .replaceAll("--username--", username)
+            .replaceAll("--threats--", threatsToLoc)
+            .replaceAll("--location--", loc)
+            .replaceAll("--phillipeIsThreat--", (threats[1].Philippe.includes(loc) ? `Threat to ${loc}` : `No Threat To ${loc}`))
+    })
+} else {
+    document.body.innerHTML = "<div storm index last><h1>You are not signed in <h1><a href='./signin'><button>Sign In</button></a></div>"
+}
+
+// ?usrnm=Delvin&loc=The%20USVI
